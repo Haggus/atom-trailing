d83ba5b30fab95db53d1493c154f5fe33ad17bfc
Mateusz 'Haggus' Mrowiec <matt.mrowiec@gmail.com>
2016-11-14 22:14:58 +0100

    Initial commit

---
+++ spec/atom-trailing-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomTrailingView from '../lib/atom-trailing-view';
+
+describe('AtomTrailingView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
